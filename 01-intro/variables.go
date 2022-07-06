4bd1dc66e98da25f9923e7c6a10199211eb3f761
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-06 14:15:46 +0530

    type conversions, const & iota

--- 01-intro/variables.go
+++ 01-intro/variables.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 //var x int = 100
 var x = 100 //unused variables at the package level are allowed (not in a function)
@@ -102,5 +105,100 @@ func main() {
 	x, y, str := 100, 200, "Result = "
 	result := x + y
 	fmt.Println(str, result)
+
+	//Complex
+	var c complex64 = 10 + 2i
+	fmt.Printf("%T\n", c)
+	fmt.Println("real = ", real(c))
+	fmt.Println("imaginary = ", imag(c))
+
+	//Rune
+	rune1 := 'B'
+	rune2 := 'g'
+	rune3 := '\a'
+
+	// Displaying rune and its type
+	fmt.Printf("Rune 1: %c; Unicode: %U; Type: %s\n", rune1,
+		rune1, reflect.TypeOf(rune1))
+
+	fmt.Printf("Rune 2: %c; Unicode: %U; Type: %s\n", rune2,
+		rune2, reflect.TypeOf(rune2))
+
+	fmt.Printf("Rune 3: Unicode: %U; Type: %s\n", rune3,
+		reflect.TypeOf(rune3))
+
+	//type conversion
+	var i int = 100
+	var f float32 = float32(i)
+	fmt.Println("f = ", f)
+
+	var f2 float32 = 1000.999
+	var no int = int(f2)
+	fmt.Println(no)
+
+	//For string to number conversion and vice versa , use strconv package
+	/*
+		x1 := "100"
+		y1 := strconv.Atoi(x1) + 100 //TO BE FIXEX
+	*/
+
+	//Constant
+	//const pi float32 = 3.14
+	const pi = 31.4
+	//pi = 2
+	fmt.Println(pi)
+
+	//iota
+	/*
+		const (
+			red   = iota
+			green = iota
+			blue  = iota
+		)
+	*/
+
+	/*
+		const (
+			red = iota
+			green
+			blue
+		)
+	*/
+
+	/*
+		const (
+			red = iota + 2
+			green
+			blue
+		)
+	*/
+
+	/*
+		const (
+			red = iota * 2
+			green
+			blue
+		)
+	*/
+
+	const (
+		red = iota * 2
+		green
+		_
+		blue
+	)
+	fmt.Println("Red =", red, "Green =", green, "Blue =", blue)
+
+	const (
+		VERBOSE = 1 << iota
+		CONFIG_FROM_DISK
+		DATABASE_REQUIRED
+		LOGGER_ACTIVATED
+		DEBUG
+		FLOAT_SUPPORT
+		RECOVERY_MODE
+		REBOOT_ON_FAILURE
+	)
+	fmt.Printf("%b, %b, %b, %b, %b, %b, %b, %b\n", VERBOSE, CONFIG_FROM_DISK, DATABASE_REQUIRED, LOGGER_ACTIVATED, DEBUG, FLOAT_SUPPORT, RECOVERY_MODE, REBOOT_ON_FAILURE)
 
 }
