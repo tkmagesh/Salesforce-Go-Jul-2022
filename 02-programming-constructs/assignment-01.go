033abebba370698df1597465ecc5009a303c3743
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-06 16:23:44 +0530

    assingments 01 & 02 - solution

--- 02-programming-constructs/assignment-01.go
+++ 02-programming-constructs/assignment-01.go
@@ -2,3 +2,22 @@
 Write a program that will print the prime numbers between start & end
 accept the start & end from the user
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var start, end int
+	fmt.Println("Enter the start and end:")
+	fmt.Scanln(&start, &end)
+LOOP:
+	for no := start; no <= end; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue LOOP
+			}
+		}
+		fmt.Println("Prime No : ", no)
+	}
+}
--- 02-programming-constructs/assignment-02.go
+++ 02-programming-constructs/assignment-02.go
@@ -21,3 +21,43 @@
 		print "Invalid choice"
 		display the menu again
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var x, y, userChoice, result int
+	for {
+		fmt.Println("Menu")
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+
+		fmt.Println("Enter your choice:")
+		fmt.Scanln(&userChoice)
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice...")
+			continue
+		}
+		if userChoice == 5 {
+			break
+		}
+		fmt.Println("Enter the values:")
+		fmt.Scanln(&x, &y)
+		switch userChoice {
+		case 1:
+			result = x + y
+		case 2:
+			result = x - y
+		case 3:
+			result = x * y
+		case 4:
+			result = x / y
+		}
+		fmt.Println("Result =", result)
+	}
+	fmt.Println("Thank you!")
+}
