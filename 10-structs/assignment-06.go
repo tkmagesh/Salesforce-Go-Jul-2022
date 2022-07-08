cac3f1abbfa1b9704c0ef0385e867ee17dd2c8a9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-08 09:49:29 +0530

    assignment-06 problem statement

---
+++ 10-structs/assignment-06.go
@@ -0,0 +1,21 @@
+/*
+	Create a product struct with the following fields
+		Id
+		Name
+		Cost
+		Units
+		Category
+
+	Create the following utility functions for the product
+		Format()
+			Returns a formatted string of the given product
+			ex : Id=100, Name="Pen", Cost=10, Units=100, Category="Stationary"
+
+		ApplyDiscount(...)
+			apply the given discount (%) for the given product
+			ex:
+				Before discount, if product cost = 10
+				apply discount of 10%
+				After discount, product cost should be 9
+
+*/
