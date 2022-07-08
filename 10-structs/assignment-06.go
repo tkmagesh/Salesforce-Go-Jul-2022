a1448d932f1740ff94e96e89b76338c59a3b9cb8
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-08 10:09:04 +0530

    assignment-06 solution

--- 10-structs/assignment-06.go
+++ 10-structs/assignment-06.go
@@ -19,3 +19,37 @@
 				After discount, product cost should be 9
 
 */
+
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Units    int
+	Category string
+}
+
+func Format(p Product) string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %v, Units = %d, Category = %q", p.Id, p.Name, p.Cost, p.Units, p.Category)
+}
+
+func ApplyDiscount(p *Product, discountPercent float32) {
+	p.Cost = p.Cost * ((100 - discountPercent) / 100)
+}
+
+func main() {
+	pen := Product{
+		Id:       100,
+		Name:     "Pen",
+		Cost:     10,
+		Units:    100,
+		Category: "Stationary",
+	}
+	fmt.Println(Format(pen))
+	fmt.Println("After applying 10% discount")
+	ApplyDiscount(&pen, 10)
+	fmt.Println(Format(pen))
+}
