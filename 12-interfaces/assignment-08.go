bfc21ba9d9fd09738a2a0f79a530d76eb74047c7
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-08 12:21:26 +0530

    assignment-08 - solution

--- 12-interfaces/assignment-08.go
+++ 12-interfaces/assignment-08.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -16,6 +17,26 @@ func main() {
 	fmt.Println(sum(10, 20, []any{30, 40, []any{10, "20"}})) //=> 130
 }
 
-func sum( /*  */ ) int {
-
+func sum(data ...any) int {
+	result := 0
+	for _, v := range data {
+		switch val := v.(type) {
+		case int:
+			result += val
+		case string:
+			if no, err := strconv.Atoi(val); err == nil {
+				result += no
+			}
+		case []any:
+			result += sum(val...)
+		case []int:
+			//result += sum(val...)
+			vals := make([]any, 0, len(val))
+			for _, x := range val {
+				vals = append(vals, x)
+			}
+			result += sum(vals...)
+		}
+	}
+	return result
 }
